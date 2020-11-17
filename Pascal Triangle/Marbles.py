596587b1ffd2e87fdd8dfb787e8e52bbef23d396
Job van Ellen <jobvanellen@kpnmail.nl>
2020-11-17 22:35:58 +0100

    changed percentages array

--- Pascal Triangle/Marbles.py
+++ Pascal Triangle/Marbles.py
@@ -1,7 +1,7 @@
 #%%
-import numpy as np  
+import numpy as np
 import random
-import matplotlib.pyplot as plt  
+import matplotlib.pyplot as plt
 
 height = 10
 width = 10.0
@@ -9,27 +9,27 @@ max_agents = 1000
 finished_agents = 0
 
 # administration per agent  
-x_pos = np.zeros((max_agents+1), np.float32)  
-y_pos = np.zeros((max_agents+1), np.uint8)  
+x_pos = np.zeros((max_agents+1), np.float32)
+y_pos = np.zeros((max_agents+1), np.uint8)
 status = np.zeros((max_agents+1), np.uint16)
 
 #column administration
 column_count = np.zeros((11), np.float32)
 
-x_pos.fill(0)  
-y_pos.fill(0)  
+x_pos.fill(0)
+y_pos.fill(0)
 status.fill(0)
 column_count.fill(0)
 time = 0
 
-def calculate_precentages(count, agents):
-    ret = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+def calculate_percentages(count, agents):
+    percentages = np.zeros((max_agents+1), np.uint32)
     for i in range(0,10):
-        ret[i] = float(count[i]/agents*100)
-        ret[i] = round(ret[i], 1)
-    return ret
+        percentages[i] = float(count[i]/agents*100)
+        percentages[i] = round(percentages[i], 1)
+    return percentages
 
-def plot_all_the_stuff():
+def plot_all_the_stuff(count, agents):
     #plot dividers
     plt.scatter([4.5],[8.5], c='r', marker='^')
     plt.scatter([4,5],[7.5,7.5], c='r', marker='^')
@@ -49,21 +49,24 @@ def plot_all_the_stuff():
     plt.yticks(np.arange(0, 10, 1.0))
 
     #keep track of percentages
-    percentages = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    if finished_agents > 0:
-        percentages = calculate_precentages(column_count, finished_agents)
+    percentages = np.zeros((max_agents+1), np.uint32)
+    if agents > 0:
+        percentages = calculate_percentages(count, agents)
     for i in range(0,10):
         plt.annotate((str(percentages[i]) + '%'), xy=(i,-.5), ha='center')
-        plt.annotate((int(column_count[i])), xy=(i,-1),ha='center')
+        plt.annotate((int(count[i])), xy=(i,-1),ha='center')
 
     plt.xlabel('Marble results')
     plt.ylabel('Falling depth')
 
+    #save plot
     #fname = 'Marbles-%03d.png' % time
     #plt.savefig('images/'+fname)
+
+    #show and close plot
     plt.show(block = False)
     plt.pause(.05)
-    plt.close
+    #plt.close()
     plt.clf()
 
 def plot_agent(x,y):
@@ -108,7 +111,7 @@ while time < 1011:
     time = time + 1
 
     #plot all the stuff & save all the plots
-    plot_all_the_stuff()
+    plot_all_the_stuff(column_count, finished_agents)
     
 
 #print column count
